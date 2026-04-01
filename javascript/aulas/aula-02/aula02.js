36acfa0721fd45cf39e698e72a1d9a54b569241b
João Othávio <joaoothaviofabri14@gmail.com>
2026-04-01 11:03:13 -0300

    Adicionando as aulas 2 e 3 de JavaScript

---
+++ javascript/aulas/aula-02/aula02.js
@@ -0,0 +1,6 @@
+"use strict" //Modo estrito
+
+let nome="João"
+console.log("Meu nome é: ")
+console.log(nome)
+console.log("Nome: " + nome)
---
+++ javascript/aulas/aula-03/aula03.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let nome="moonlight"
+nome="yeah"
+nome=24
+
+const curso="JavaScript"
+
+curso="HTML"
+
+console.log(curso)
