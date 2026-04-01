3d4a3511f5fcf4af4b6f3b4324d74eb8ad0d87bc
João Othávio <joaoothaviofabri14@gmail.com>
2026-04-01 16:07:06 -0300

    Adicionando a aula 07 de JavaScript

---
+++ javascript/aulas/aula-07/aula07.js
@@ -0,0 +1,14 @@
+// && -> and e
+// || -> or ou
+// ! -> not nao
+let numero1, numero2, numero3, numero4
+numero1 = 10
+numero2 = 5
+numero3 = 15
+numero4 = 2
+
+if( (numero1 > numero2) && (numero3 > numero4) ){   
+    console.log("verdadeiro")
+}else{
+    console.log("falso")
+}
