4f452b58cb1f80e587b5124e9991c8cf2a2f6778
João Othávio <joaoothaviofabri14@gmail.com>
2026-04-16 16:36:33 -0300

    Adicionando a aula 10 de JavaScript

---
+++ javascript/aulas/aula-10/aula10.js
@@ -0,0 +1,14 @@
+let num = 10
+let num2 = 5
+res = (! (num % 2) ? "Par" : "Impar")
+console.log(res)
+
+let st = "A"
+res = (st == "A" ? "Ativo" : "Inativo")
+console.log(res)
+
+//0 = False
+//1 - True
+
+// Teste lógico ? se verdadeiro : se falso
+
