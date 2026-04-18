f4b71dc293fc5227e918041eb4dbee39e1ee4df5
João Othávio <joaoothavio@tutamail.com>
2026-04-18 19:58:27 -0300

    Adicionando a aula 15 de JavaScript

---
+++ javascript/aulas/aula-15/aula15.js
@@ -0,0 +1,15 @@
+console.log("Início do programa")
+
+for (let i = 0; i < 100; i++) {
+    if(i % 2 == 0) {
+        console.log(`${i} é par`)
+    } else {
+        console.log(`${i} é impar`)
+    }
+}
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Curso de JavaScript - valor do i: ${i}`)
+// }
+
+console.log("Fim do programa")
