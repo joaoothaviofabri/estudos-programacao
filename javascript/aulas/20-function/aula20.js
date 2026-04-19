df2620ea5078dc7e410993cd0f5aa8ddb157f324
João Othávio <joaoothavio@tutamail.com>
2026-04-19 18:47:51 -0300

    Adicionando a aula 20 e reorganização nos nomes das pastas

---
+++ javascript/aulas/20-function/aula20.js
@@ -0,0 +1,27 @@
+function mudarTexto() {
+    let d1 = document.getElementById("d1")
+    let d2 = document.getElementById("d2")
+    let d3 = document.getElementById("d3")
+    d1.innerHTML = "Aprendendo JavaSrcipt"
+    d2.innerHTML = "Aprendendo JavaSrcipt"
+    d3.innerHTML = "Aprendendo JavaSrcipt"
+}
+
+// function soma2_10() {
+//     let n1 = 2
+//     let n2 = 10
+//     let soma = n1 + n2
+//     console.log(soma)
+// }
+
+// for(let i = 0; i < 10; i++) {
+//     soma2_10()
+// }
+
+// function nome() {
+//     console.log("Aprendendo JavaScript")
+// }
+
+// for(let i = 0; i < 10; i++) {
+//     nome()
+// }
