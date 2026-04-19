7c422aafa1d62c23a4a1e074197acf1a58b137de
João Othávio <joaoothavio@tutamail.com>
2026-04-19 18:07:07 -0300

    Adicionando a aula 17 de JavaScript

---
+++ javascript/aulas/aula-17/aula17.js
@@ -0,0 +1,19 @@
+// 5! = 5*4*3*2*1 = 120
+
+let n = 5
+let fat = 1
+
+while(n >= 1) {
+    fat *= n
+    // fat = fat * n
+    n--
+}
+
+console.log(fat)
+
+// let n = 0
+
+// while(n < 10) {
+//     console.log(n)
+//     n++
+// }
