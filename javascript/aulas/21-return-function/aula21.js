328aa0d671b94cf18065dacdee20c7016127ef64
João Othávio <joaoothavio@tutamail.com>
2026-04-19 20:47:36 -0300

    21 - Funções com Retorno em JS

---
+++ javascript/aulas/21-return-function/aula21.js
@@ -0,0 +1,14 @@
+function curso() {
+    let n1 = 10
+    let n2 = 2
+    let res = n1 * n2
+    if(res % 2 == 0) 
+        return "Par"
+    else
+        return "Impar"
+}
+
+let res = curso()
+
+console.log(res)
+// console.log(curso())
