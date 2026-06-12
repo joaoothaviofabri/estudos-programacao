e3d42370946f3e48db8367ea4e71abb486549d5d
joaoothavio <joaoothavio@tutamail.com>
2026-06-12 16:43:50 -0300

    Strings e Variáveis em ruby.

---
+++ ruby/pratica/string-variaveis/app.rb
@@ -0,0 +1,17 @@
+# puts (print)
+# Sintaxe:
+# puts "Olá, Mundo!"
+
+# Forma de "puts" pelo terminal:
+# ruby 'nome-do-arquivo.rb'
+# ou 
+# irb > puts 'Hello World!'
+
+# Variável
+# variavel = valor / "valor"
+# puts com valor de variável
+
+nome = gets.chomp
+idade = gets.chomp
+
+puts "#{nome} tem #{idade} anos."
