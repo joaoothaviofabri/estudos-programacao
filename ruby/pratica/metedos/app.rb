76f9f1da63fce8dc92e92cacec3bf16ab679babd
joaoothavio <joaoothavio@tutamail.com>
2026-06-17 14:42:05 -0300

    Métodos em ruby.

---
+++ ruby/pratica/metedos/app.rb
@@ -0,0 +1,20 @@
+def soma x, y
+    x + y
+end
+
+def subt(x, y)
+    x - y
+end
+
+def mult x, y
+    x * y
+end
+
+def divi(x, y)
+    x / y
+end
+
+puts soma 2, 4
+puts subt 5, 6
+puts mult 3, 9
+puts divi 10, 5
