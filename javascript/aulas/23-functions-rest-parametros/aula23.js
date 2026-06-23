b5997cef216fb69a992dfa7d0b569227eb9bff36
joaoothavio <joaoothavio@tutamail.com>
2026-06-23 13:00:00 -0300

    Funções com parametros em javascript.

---
+++ javascript/aulas/23-functions-rest-parametros/aula23.js
@@ -0,0 +1,50 @@
+function add(v) {
+    return valor += v
+}
+
+let valor = 0
+console.log(valor)
+
+valor = add(10)
+console.log(valor)
+
+valor = add(5)
+console.log(valor)
+
+// const VALOR_PADRAO = 0
+
+// function soma(n1 = VALOR_PADRAO, n2 = VALOR_PADRAO) {
+//     let res
+//     res = n1 + n2
+//     return res
+// }
+
+// let resultado_soma = soma(5, 5)
+// console.log(resultado_soma)
+
+// function soma(n1= 0, n2 = 0) {
+//     let res
+//     res = n1 + n2
+//     return res
+// }
+
+// let resultado_soma = soma(5, 5)
+// console.log(resultado_soma)
+// console.log(soma(10, 10))
+// soma(10, 10)
+
+
+// function soma(n1 = 0, n2 = 0) {
+//     console.log(n1 + n2)
+// }
+
+// soma(10)
+// soma(10, 5)
+
+// function soma(p1) {
+//     console.log(p1)
+// }
+
+// soma("João")
+// soma(2022)
+// soma(5.2)
